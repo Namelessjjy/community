5631b90b8b4be2497001624e39f629ba7e5b6719
NamelessJJY <505040313@qq.com>
2020-02-17 21:07:50 +0800

    add check cookies

--- src/main/java/com/photo/community/mapper/UserMapper.java
+++ src/main/java/com/photo/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.photo.community.mapper;
 import com.photo.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 
 /**
@@ -22,4 +24,7 @@ public interface UserMapper {
 
     @Insert("insert into user (name, account_id, token, gmt_create, gmt_modified) value (#{name}, #{accountId}, #{token}, #{gmtCreate}, #{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token=#{token}")
+    User findByToken(@Param("token") String token);
 }
